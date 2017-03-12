60636dc65810338949cefc45250fc85be1e8ce4a
Andrew Stewart <andrew@stwrt.ca>
2017-03-11 19:59:40 -0800

    Add babel-loader, include loader ruleset

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,20 @@ const path = require("path");
 
 let config = {
   entry: "./src/index.js",
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
   },
 };
 
