c20ada6cb08a0d21ba6ac018a921c159dc98f5c4
Andrew Stewart <andrew@stwrt.ca>
2017-03-11 19:51:15 -0800

    Add a number of additional eslint rules

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ let config = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build")
-  }
+    path: path.resolve(__dirname, "build"),
+  },
 };
 
 module.exports = config;
